aa435456dc71cd3bb4299e637e0c27c1977c6a23
shun4559 <kiuchi4559@gmail.com>
2026-04-04 23:58:31 +0900

    try  catch操作追加

--- app/actions/deleteItem.ts
+++ app/actions/deleteItem.ts
@@ -4,8 +4,13 @@ import { revalidatePath } from "next/cache";
 import { supabase } from "../lib/supabase";
 
 export async function deleteItem(formData: FormData) {
-  const id = formData.get("id");
+ 
+try{
+ const id = formData.get("id");
   const response = await supabase.from("todos").delete().eq("id", id);
-
-  revalidatePath("/");
+   revalidatePath("/");
+}catch(err){
+  console.log(err)
+}
+ 
 }
